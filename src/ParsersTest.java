487aaf0a21f9fc57a7a3b2578a2628e62fc39c76
Jakub A. Gramsz <jakub.gramsz@gmail.com>
2015-05-19 01:24:24 +0200

    fst uts

--- src/ParsersTest.java
+++ src/ParsersTest.java
@@ -1,15 +1,30 @@
+import Monad.ListMonad;
+import Primitives.Pair;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import Primitives.Div;
 import Primitives.Con;
 
 /**
- * Created by olorin on 13.05.15.
+ * Created by Olórin on 13.05.15.
+ *
  */
 public class ParsersTest {
     @Test
     public void testTermSimple() {
-        assertEquals( new Div<>(new Con<>(1), new Con<>(2)),
-                      Prs.term().parse("(1/2)").get(0).fst );
+        assertEquals(new Div<>(new Con<>(1), new Con<>(2)),
+                Prs.term().parse("(1/2)").get(0).fst);
+    }
+    @Test
+    public void testPrsLetterInd() {
+        assertEquals((Character) 'N',
+                Prs.letter.parse("Napis").get(0).fst);
+    }
+    @Test
+    public void testPrsLetterNull() {
+        assertEquals(new ListMonad<Pair<Character,String>>(),
+                Prs.letter.parse("1234"));
     }
 }
